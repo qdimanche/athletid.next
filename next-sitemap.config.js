f4dd0445757e33a38d25de0680137683d959515a
quentindimanche <qdimanche@gmail.com>
2023-01-20 16:31:40 +0100

    bug fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,6 @@
-const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
-
-
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://athletid.com',
     generateRobotsTxt: true, // (optional)
     // ...other options
-    additionalSitemaps: [
-        `${siteUrl}sitemap.xml`,
-        `${siteUrl}server-sitemap.xml`,
-    ],
 }
