14c12efc474d7f1ae4fd5be572798e397394e06c
quentindimanche <qdimanche@gmail.com>
2023-01-20 15:05:57 +0100

    [+] favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Script from "next/script"
 
 export default function App({ Component, pageProps }) {
   return <>
+      <link rel="icon" href="%PUBLIC_URL%/favicon.ico"/>
       <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-F3ZEBZ00CK"/>
       <Script
           id='google-analytics'
--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ export default function Home() {
     return (<>
         <Head>
             <meta charSet="utf-8"/>
-            <link rel="icon" href="%PUBLIC_URL%/favicon.ico"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <meta name="theme-color" content="#000000"/>
             <meta
