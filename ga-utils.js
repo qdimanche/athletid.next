bff701fbf3f5c9e6a4d0f64cfe0ca9174ba0b3e8
quentindimanche <qdimanche@gmail.com>
2023-01-27 14:49:50 +0100

    bug fix

--- ga-utils.js
+++ ga-utils.js
@@ -1,7 +1,5 @@
 import * as ReactGA from "react-ga";
 
 export const initGA = (id) => {
-    if (process.env.NODE_ENV === "production") {
-        ReactGA.initialize(id);
-    }
+    ReactGA.initialize(id);
 };
