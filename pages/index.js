7d18f9c1df96d9ccd4fe13f258ef164d668f7683
quentindimanche <qdimanche@gmail.com>
2023-01-27 16:02:29 +0100

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import ImgHeaderHome from "../public/assets/images/femme-entrainement-exterieur-
 import timerAppPreview from "../public/assets/images/application-athletid-entrainement-sportif.webp";
 import athletidAppPreview from "../public/assets/images/application-athletid.webp"
 import athletidAppPreview2 from "../public/assets/images/application-athletid-construction.webp";
-import femmeEntrainement from "../public/assets/videos/femme-entrainement-exterieur-fitness.mp4";
 import appSNVideo from "../public/assets/videos/application-network-athletid-fonctionnalites.mp4";
 import appTimerVideo from "../public/assets/videos/application-timer-athletid-fonctionnalites.mp4";
 import {FormattedMessage, useIntl} from "react-intl";
@@ -39,7 +38,7 @@ export default function Home() {
 			/>
 			<FullScreenBgImage
 				src={athletidAppPreview}
-				video={appSNVideo}
+				video={"https://res.cloudinary.com/dxplbf0t0/video/upload/v1674831669/application-network-athletid-fonctionnalites_enqkou.mp4"}
 				title={"Partage ta \n passion !"}
 				button={true}
 				subtitle={"Enregistre tes séances et partage ta vie de sportif avec une communauté qui te ressemble. Un lieu inspirant, accessible à tous, empreint de valeurs sportives, avec un but commun : atteindre ensemble nos objectifs !"}
@@ -49,7 +48,7 @@ export default function Home() {
 			/>
 			<FullScreenBgImage
 				src={timerAppPreview}
-				video={appTimerVideo}
+				video={"https://res.cloudinary.com/dxplbf0t0/video/upload/v1674831668/application-timer-athletid-fonctionnalites_mszyz3.mp4"}
 				title={"Ton temps \n est précieux !"} button={false} tag={"Bientôt disponible"}
 				subtitle={"Paramètre ton entraînement et laisse toi guider. Enchaîne tes exercices l’esprit libre et respecte chaque seconde pour te rapprocher un peu plus de ton objectif."}
 				link={"/network"}
