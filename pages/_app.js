829e6bfd71998022b28148dfd4991154f93a5494
quentindimanche <qdimanche@gmail.com>
2023-01-27 14:21:26 +0100

    bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -36,21 +36,6 @@ function App({Component, pageProps}) {
 	
 	return <>
 		<link rel="icon" href="%PUBLIC_URL%/favicon.ico"/>
-		<Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-F3ZEBZ00CK"/>
-		<Script
-			id='google-analytics'
-			strategy="afterInteractive"
-			dangerouslySetInnerHTML={{
-				__html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-F3ZEBZ00CK', {
-            page_path: window.location.pathname,
-          });
-        `,
-			}}
-		/>
 		
 		<IntlProvider locale={locale} messages={messages[locale]}>
 			<Component {...pageProps} />
