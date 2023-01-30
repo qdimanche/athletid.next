51d2133ad1899bc4922e2bfed53334df8d79e9f2
quentindimanche <qdimanche@gmail.com>
2023-01-30 09:48:41 +0100

    bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '@/styles/globals.css'
-import Script from "next/script"
 import en from "../lang/en.json";
 import fr from "../lang/fr.json";
 import {useRouter} from "next/router";
@@ -7,6 +6,7 @@ import {IntlProvider} from "react-intl";
 import CookieConsent, {Cookies, getCookieConsentValue} from "react-cookie-consent";
 import {initGA} from "@/ga-utils";
 import {useEffect} from "react";
+import Link from "next/link";
 
 const messages = {
 	en, fr,
@@ -15,9 +15,7 @@ const messages = {
 function App({Component, pageProps}) {
 	const {locale} = useRouter();
 	const handleAcceptCookie = () => {
-		if (process.env.REACT_APP_GOOGLE_ANALYTICS_ID) {
-			initGA(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
-		}
+		initGA("UA-254205972-1");
 	};
 	
 	const handleDeclineCookie = () => {
@@ -36,32 +34,35 @@ function App({Component, pageProps}) {
 	
 	return <>
 		<link rel="icon" href="%PUBLIC_URL%/favicon.ico"/>
-		<Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-F3ZEBZ00CK"/>
-		<Script
-			id='google-analytics'
-			strategy="afterInteractive"
-			dangerouslySetInnerHTML={{
-				__html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-F3ZEBZ00CK', {
-            page_path: window.location.pathname,
-          });
-        `,
-			}}
-		/>
 		
 		<IntlProvider locale={locale} messages={messages[locale]}>
 			<Component {...pageProps} />
 		</IntlProvider>
 		
 		<CookieConsent
+			style={{
+				background: '#F5F4F5', color: 'black'
+			}}
+			buttonStyle={{
+				borderRadius: '8px ',
+				backgroundColor: 'black',
+				color: 'white',
+				margin: '15px 10px'
+			}}
+			buttonText={'Accepter'}
+			declineButtonStyle={{
+				borderRadius: '8px ',
+				backgroundColor: '#E63D31',
+				color: 'white',
+				margin:'15px 20px 15px 10px'
+			}}
+			contentClasses={'!mb-0'}
+			declineButtonText={'Refuser'}
 			enableDeclineButton
-			onAccept={handleAcceptCookie}>
+			onAccept={handleAcceptCookie}
 			onDecline={handleDeclineCookie}
-			This website uses cookies to enhance the user
-			experience.
+			flipButtons>
+			Ce site web utilise des cookies pour améliorer votre expérience &apos;utilisateur, pour en savoir plus cliquer <Link href={'/politique-de-confidentialite'} className={'font-bold'}>ici</Link>.
 		</CookieConsent>
 	
 	
