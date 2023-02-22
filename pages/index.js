56568dce092388c403a8a32dd8196c9fc760f382
qdimanche <73292719+qdimanche@users.noreply.github.com>
2023-02-22 17:34:47 +0100

    [+]bug fix

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
+import FullScreenBgImage from '../src/components/UI/FullScreenBgImage'
+import MasonryGrid from '../src/components/UI/MasonryGrid'
 import ContactSupport from '../src/components/Contact/ContactSupport'
+import timerAppPreview from '../public/assets/images/application-athletid-entrainement-sportif.webp'
+import capteurAppPreview from '../public/assets/images/capteur-athletid-screenshot.png'
 import statistiquesApp from '../public/assets/images/statistiques-application-athletid.webp'
 import {useIntl} from 'react-intl'
 import Format from '../src/layout/format'
 import Header from '@/src/components/Header/HeaderJustifySelfEnd'
 import ImgHeader from '@/public/assets/images/femme-casque-audio.webp'
 import useMediaQuery from "@/src/components/Hooks/useMediaQuery";
-import GridLayout from "@/src/components/Home/GridLayout";
 
 export default function Home() {
     const intl = useIntl()
@@ -34,7 +37,28 @@ export default function Home() {
                     instaLink={'https://www.instagram.com/athletid/'}
                     color={'white'}
                 />
-                <GridLayout/>
+                <FullScreenBgImage
+                    src={timerAppPreview}
+                    video={'https://res.cloudinary.com/dxplbf0t0/video/upload/v1674831668/application-timer-athletid-fonctionnalites_mszyz3.mp4'}
+                    title={"Bien plus qu'un \nsimple timer."}
+                    tag={'Bientôt disponible'}
+                    subtitle={'Paramètre ton entraînement et laisse-toi guider. Enchaîne tes exercices l’esprit libre et respecte chaque seconde pour te rapprocher un peu plus de ton objectif.'}
+                    link={'/timer'}
+                />
+                <FullScreenBgImage
+                    src={capteurAppPreview}
+                    video={'https://res.cloudinary.com/dxplbf0t0/video/upload/v1675439914/captor_erchao.mp4'}
+                    title={'Objets connectés. \nApplication.'}
+                    button={'hidden'}
+                    subtitle={"Nous voulons permettre aux adeptes de fitness, yoga et sports connexes de progresser et d'atteindre leurs objectifs.\n" + 'Pour cela nous développons un univers unique et dédié à ces disciplines.'}
+                    tag={'À venir'}
+                    idSection={'networkHeader'}
+                    link={'/'}
+                />
+                <MasonryGrid
+                    titleMasonry={'Accompagner.\n Innover. Progresser.'}
+                    subTitleMasonry={'Construisons ensemble l’univers qui vous propulsera vers vos objectifs.'}
+                />
                 <ContactSupport src={statistiquesApp}/>
             </Format>
         </>)
