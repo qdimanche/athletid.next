31f03b7e0c92dedd04d3d41aef7893933b6b6201
qdimanche <73292719+qdimanche@users.noreply.github.com>
2023-04-06 15:19:29 +0200

    change baseURL

--- src/components/Blog/HeaderBlog.js
+++ src/components/Blog/HeaderBlog.js
@@ -4,7 +4,7 @@ import 'swiper/css'
 const HeaderBlog = () => {
     return (
         <div className={'mt-[142px] md:mt-[216px]'}>
-            <h1 className={'mb-4'}>Tips and workouts to reach your goals.</h1>
+            <h1 className={'mb-4'}>Tips and <br/>workouts to <br/>reach your goals.</h1>
             <p>
                 We won&apos;t be able to help you become as strong and fast as Superman, but
                 we have some great articles!
