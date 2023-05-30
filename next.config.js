dfce5027c12e53675fed63b0c5c9eabc090cb33a
qdimanche <73292719+qdimanche@users.noreply.github.com>
2023-05-30 10:37:50 +0200

    correct paths for images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@ const {i18n} = require("./next-i18next.config");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: [
+        'res.cloudinary.com',
+        'athletid.com'
+    ],
   },
   i18n
 }
